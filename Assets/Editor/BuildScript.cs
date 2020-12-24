1e2cefba7cd5b97a4961033658a60c6a7fec1dc4
CY <cycleapple@gmail.com>
2020-12-24 21:57:30 +0800

    fix: remove namespace and change class name

---
+++ Assets/Editor/BuildScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using UnityEditor;
+
+public class CI
+{
+    [MenuItem("CI/Build")]
+    public static void Build()
+    {
+#if UNITY_EDITOR
+        BuildPipeline.BuildPlayer(ScenePaths, "Build/TestProject.exe", BuildTarget.StandaloneWindows64,
+            BuildOptions.None);
+#endif
+    }
+
+    static string[] ScenePaths => EditorBuildSettings.scenes.Select(scene => scene.path).ToArray();
+}
