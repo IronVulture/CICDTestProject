6c9c865345d9cb913ccd65658926008244a0c832
CY <cycleapple@gmail.com>
2020-12-25 01:45:37 +0800

    feat: add switch test script

--- Assets/Editor/BuildScript.cs
+++ Assets/Editor/BuildScript.cs
@@ -3,12 +3,21 @@ using UnityEditor;
 
 public class CI
 {
-    [MenuItem("CI/Build")]
-    public static void Build()
+    [MenuItem("CI/Win64Build")]
+    public static void Win64Build()
     {
 #if UNITY_EDITOR
         BuildPipeline.BuildPlayer(ScenePaths, "Build/TestProject.exe", BuildTarget.StandaloneWindows64,
             BuildOptions.None);
+#endif
+    }
+    [MenuItem("CI/SwitchBuild")]
+    static void SwitchBuild()
+    {
+
+#if UNITY_EDITOR
+        // Build player.
+        BuildPipeline.BuildPlayer(ScenePaths, "NSbuilds/TestProject", BuildTarget.Switch, BuildOptions.Development);
 #endif
     }
 
